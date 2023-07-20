fe34f15864168925eaf09b363041162b2c841342
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2023-07-19 23:58:17 -0500

    wip: Add JULIA_CC environment variable

--- recipes/haplink/build.sh
+++ recipes/haplink/build.sh
@@ -3,6 +3,11 @@
 which gcc
 
 export JULIA_CPU_TARGET="x86-64"
+if [[ "$(uname -s)" == "Linux" ]]; then
+  export JULIA_CC=$(which x86_64-conda-linux-gnu-gcc)
+else
+  export JULIA_CC=$(which clang)
+fi
 
 julia --project -e 'using Pkg; Pkg.instantiate()'
 julia --project "deps/build.jl" app
