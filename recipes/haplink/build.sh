3d090c2364f8cb7290b4dc38b532ffcbfa765a86
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2023-08-11 12:04:39 -0500

    wip: Create 'stacked depot'

--- recipes/haplink/build.sh
+++ recipes/haplink/build.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+# Avoid libgit2 issues as much as humanly possible
 export JULIA_PKG_USE_CLI_GIT=true
 
 # Certificate error workaround:
 # https://discourse.julialang.org/t/package-fetch-issues-with-internal-ssl-certificate/43026/2
-# mv ${JULIA_DIR}/share/julia/cert.pem ${JULIA_DIR}/share/julia/cert.pem.bak
-# ln -s /etc/ssl/certs/ca-certificate.crt ${JULIA_DIR}/share/julia/cert.pem
 if [[ $OSTYPE != 'darwin'* ]]; then
-  JULIA_BIN_DIR=$(dirname $(which julia))
-  JULIA_SHARE_DIR=${JULIA_BIN_DIR}/../share/julia
-  mkdir -p ${JULIA_SHARE_DIR}
-  cp $JULIA_SSL_CA_ROOTS_PATH ${JULIA_SHARE_DIR}/cert.pem
+  JULIA_DEFAULT_DEPOT=$(echo "$JULIA_DEPOT_PATH" | cut -d : -f1)
+  cp "${JULIA_SSL_CA_ROOTS_PATH}" "${JULIA_DEFAULT_DEPOT}/cert.pem"
 fi
 
+# Create a new depot for HapLink to install into
+HAPLINK_JULIA_DEPOT_PATH="${PREFIX}/share/haplink"
+JULIA_DEPOT_PATH="${HAPLINK_JULIA_DEPOT_PATH}:$JULIA_DEPOT_PATH"
+
+# Run the Comonicon install method
 julia --project -e 'using Pkg; Pkg.instantiate()'
 julia --project "deps/build.jl"
 
-mkdir -p ${PREFIX}/bin
+# Copy the script to someplace more permanent
+mkdir -p "${PREFIX}/bin"
+mv "${HAPLINK_JULIA_DEPOT_PATH}/bin/haplink" "${PREFIX}/bin"
